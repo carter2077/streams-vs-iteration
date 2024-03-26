9efd2fb3fe56710587baeb013efce34375d96b4d
carter2077 <carter2077@pm.me>
2024-03-25 23:13:59 -0400

    Enhancements

--- CityStateStreamsTest.java
+++ CityStateStreamsTest.java
@@ -4,20 +4,27 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class CityStateStreamsTest {
+    // arg0: number of states
+    // arg1: number of cities per states
+    // arg2: number of testing rounds
     public static void main(String... args) {
         // Setup
-        Integer numOfStates = 100000;
-        Integer numOfCitiesPerState = 100;
-        int rounds = 20;
+        Integer numOfStates = args[0] != null ? Integer.valueOf(args[0]) : 100000;
+        Integer numOfCitiesPerState = args[1] != null ? Integer.valueOf(args[1]) : 100;
+        Integer rounds = args[2] != null ? Integer.valueOf(args[2]) : 20;
         Random rnd = new Random();
         List<State> states = new ArrayList<State>();
 
         List<City> newCities;
         State newState;
-        for(int i = 0; i < numOfStates; i++) {
+        println("Populating states...");
+        for(Integer i = 0; i < numOfStates; i++) {
+            if(i > 0 && i % 20000 == 0) {
+                println("...");
+            }
             newState = new State(UUID.randomUUID().toString());
             newCities = new ArrayList<City>();
-            for(int j = 0; j < numOfCitiesPerState; j++) {
+            for(Integer j = 0; j < numOfCitiesPerState; j++) {
                 newCities.add(
                         new City(
                             UUID.randomUUID().toString(), 
@@ -30,9 +37,9 @@ public class CityStateStreamsTest {
         }
         
         List<BigDecimal> results = new ArrayList<BigDecimal>();
-        for(int i = 1; i <= rounds; i++) {
+        for(Integer i = 1; i <= rounds; i++) {
             BigDecimal result = doTest(states);
-            System.out.println("Round " + i + ": Stream was " + result.doubleValue() + "% faster");
+            println("Round " + i + ": Stream was " + result.doubleValue() + "% faster");
             results.add(result);
         }
         // Maybe try to stick with big decimal instead of doing this
@@ -40,13 +47,14 @@ public class CityStateStreamsTest {
         DoubleStream ds = bds.mapToDouble(bd -> bd.doubleValue());
         OptionalDouble avg = ds.average();
 
-        System.out.println("Streams averaged " + avg.getAsDouble() + "% faster");
+        println("Streams averaged " + 
+                new BigDecimal(avg.getAsDouble()).setScale(2, RoundingMode.UP) + "% faster");
     }
 
     private static BigDecimal doTest(List<State> states) {
         // Iterative
-        System.out.println("Start iterative");
-        long startTime = System.currentTimeMillis();
+        println("Start iterative round");
+        Long startTime = System.currentTimeMillis();
 
         Integer iterTotal = 0;
         for(State state: states) {
@@ -55,13 +63,13 @@ public class CityStateStreamsTest {
             }
         }
 
-        long endTime = System.currentTimeMillis();
-        long iterTime = endTime - startTime;
-        System.out.println("Iter total: " + iterTotal);
-        System.out.println("End iterative: " + iterTime);
+        Long endTime = System.currentTimeMillis();
+        Long iterTime = endTime - startTime;
+        println("Iteration counted " + iterTotal + " cities.");
+        println("End iterative round; took " + iterTime + " ms");
 
         // Stream based
-        System.out.println("Start stream based");
+        println("Start stream round");
         startTime = System.currentTimeMillis();
 
         int streamTotal = 
@@ -70,14 +78,23 @@ public class CityStateStreamsTest {
                 .mapToInt(City::getPopulation)
                 .sum();
 
-        System.out.println("Stream total: " + streamTotal);
+        println("Stream API counted " + streamTotal + "cities.");
         endTime = System.currentTimeMillis();
-        long streamTime = endTime - startTime;
-        System.out.println("End stream based: " + streamTime);
-        long diff = iterTime - streamTime;
-        double pctInc = ((double) diff) / ((double) iterTime);
-        BigDecimal res = new BigDecimal(pctInc);
-        res = res.multiply(BigDecimal.valueOf(100));
-        return res.setScale(2, RoundingMode.UP);
+        Long streamTime = endTime - startTime;
+        println("End stream round; took " + streamTime + " ms");
+
+        // Comparison
+        Long diff = iterTime - streamTime;
+        if(iterTime != 0) {
+            Double pctInc = Double.valueOf(diff) / Double.valueOf(iterTime);
+            BigDecimal res = new BigDecimal(pctInc);
+            res = res.multiply(BigDecimal.valueOf(100));
+            return res.setScale(2, RoundingMode.UP);
+        }
+        return new BigDecimal(0);
+    }
+
+    private static void println(String s) {
+        System.out.println(s);
     }
 }
